7f0079919f57a48a6d010f740b0be6c6ba42194b
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2025-03-25 08:53:12 +0900

    0724-FIND-PIVOT-INDEX Stats: Time: 0 ms (100%), Space: 9.8 MB (16.86%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.c
+++ 0724-find-pivot-index/0724-find-pivot-index.c
@@ -8,8 +8,9 @@ int pivotIndex(int *nums, int numsSize) {
     // leftSum is totalSum - previous elements sum,
     // so we can calculate rightSum by totalSum - leftSum - nums[i]
     int leftSum = 0;
+    int rightSum = 0;
     for (int i = 0; i < numsSize; i++) {
-        int rightSum = totalSum - leftSum - nums[i];
+        rightSum = totalSum - leftSum - nums[i];
         if (leftSum == rightSum) {
             return i;
         }
