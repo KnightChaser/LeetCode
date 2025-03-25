ff3d84d4570e0df809d415bec38c18d3e8d53d9d
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2025-03-25 22:30:08 +0900

    0088-MERGE-SORTED-ARRAY Stats: Time: 0 ms (100%), Space: 10 MB (62.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,12 +1,23 @@
-#include <stdlib.h>
-
-int compare(const void *a, const void *b) { return *(int *)a - *(int *)b; }
-
 void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n) {
-    // Just merge the two arrays and sort them
-    for (int i = 0; i < n; i++) {
-        nums1[m + i] = nums2[i];
+    int len = m + n;    // The total length of the merged array (also nums1)
+    int index1 = m - 1; // The last valid element of nums1
+    int index2 = n - 1; // The last valid element of nums2
+
+    // Continue while there are elements in both arrays
+    for (int i = len - 1; i >= 0 && index1 >= 0 && index2 >= 0; i--) {
+        // Place the greater element at the end of the merged array
+        if (nums1[index1] > nums2[index2]) {
+            nums1[i] = nums1[index1];
+            index1--;
+        } else {
+            nums1[i] = nums2[index2];
+            index2--;
+        }
     }
 
-    qsort(nums1, m + n, sizeof(int), compare);
+    // There's a possibility that there are still elements in nums2
+    while (index2 >= 0) {
+        nums1[index2] = nums2[index2];
+        index2--;
+    }
 }
