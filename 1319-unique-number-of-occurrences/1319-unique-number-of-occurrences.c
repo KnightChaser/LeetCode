c49f3d2e14c916bf9c1833a170052bf56759ee7e
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2025-03-25 10:02:05 +0900

    1319-UNIQUE-NUMBER-OF-OCCURRENCES Stats: Time: 0 ms (100%), Space: 9 MB (50.91%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.c
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.c
@@ -11,15 +11,25 @@ bool uniqueOccurrences(int *arr, int arrSize) {
         count[arr[i] + 1000]++;
     }
 
-    // Second, count the occurrences of each occurrence
-    //         by sorting the occurrences of each number,
-    //         if two adjacent numbers are the same, it means duplicity.
-    qsort(count, 2001, sizeof(int), compare);
-    for (int i = 1; i < 2001; i++) {
-        if (count[i] == count[i - 1] && count[i] != 0) {
-            return false;
+    // Second, allocate the boolean array to track which occurrences have been
+    // seen, the maximum number of occurrences is arrSize
+    bool *seen = (bool *)calloc(sizeof(bool) * (arrSize + 1), sizeof(bool));
+    if (seen == NULL) {
+        return false;
+    }
+
+    // Last, check if the number of occurrences is unique
+    for (int i = 0; i < 2001; i++) {
+        if (count[i] > 0) {
+            if (seen[count[i]]) {
+                // The number of occurrences is not unique
+                free(seen);
+                return false;
+            }
+            seen[count[i]] = true;
         }
     }
 
+    free(seen);
     return true;
 }
