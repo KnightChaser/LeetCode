ced529ee8e8e7769f89fc2e68fbe51daf59ddb5d
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2025-03-29 02:40:47 +0900

    0111-MINIMUM-DEPTH-OF-BINARY-TREE Stats: Time: 21 ms (15.11%), Space: 80.4 MB (63.34%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.c
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.c
@@ -9,37 +9,27 @@
 
 #include <stdio.h>
 
-int calculateMinDepth(struct TreeNode *root, int depth) {
-    // Debug print
-    printf("Node: %d, Depth: %d\n", root->val, depth);
-
-    // If the left child is NULL, then the depth is the depth of the right child
-    if (root->left == NULL && root->right == NULL) {
-        return depth;
-    }
-
-    // If left child is NULL, then the depth is the depth of the right child
-    if (root->left == NULL) {
-        return calculateMinDepth(root->right, depth + 1);
-    }
-
-    // If right child is NULL, then the depth is the depth of the left child
-    if (root->right == NULL) {
-        return calculateMinDepth(root->left, depth + 1);
-    }
-
-    // If both children are not NULL, then the depth is the minimum of the left
-    // and right children
-    int leftDepth = calculateMinDepth(root->left, depth + 1);
-    int rightDepth = calculateMinDepth(root->right, depth + 1);
-
-    return leftDepth < rightDepth ? leftDepth : rightDepth;
-}
-
 int minDepth(struct TreeNode *root) {
+    // If the root is NULL, return 0
     if (root == NULL) {
         return 0;
     }
 
-    return calculateMinDepth(root, 1);
+    // If the root has only right child,
+    // compute depth solely on the right child
+    if (root->left == NULL) {
+        return minDepth(root->right) + 1;
+    }
+
+    // If the root has only left child
+    // compute depth solely on the left child
+    if (root->right == NULL) {
+        return minDepth(root->left) + 1;
+    }
+
+    // If both children are present, compute the depth,
+    // and take the minimum of the two
+    int leftDepth = minDepth(root->left);
+    int rightDepth = minDepth(root->right);
+    return leftDepth < rightDepth ? leftDepth + 1 : rightDepth + 1;
 }
