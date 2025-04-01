98e03eb1248afab9b6830f019a6db7584dce6056
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2025-04-01 13:09:06 +0900

    0226-INVERT-BINARY-TREE Stats: Time: 0 ms (100%), Space: 8.7 MB (14.78%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.c
+++ 0226-invert-binary-tree/0226-invert-binary-tree.c
@@ -8,13 +8,19 @@
  */
 
 #include <stdio.h>
+
 struct TreeNode *invertTree(struct TreeNode *root) {
     if (root == NULL) {
         return NULL;
     }
 
+    // Swap the left and right children of the current node (level)
     struct TreeNode *temp = root->left;
-    root->left = invertTree(root->right);
-    root->right = invertTree(temp);
+    root->left = root->right;
+    root->right = temp;
+
+    invertTree(root->left);
+    invertTree(root->right);
+
     return root;
 }
