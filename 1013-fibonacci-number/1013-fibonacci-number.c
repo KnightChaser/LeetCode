ebf1ee986aabb3bce9ff679cfc678b303bbbad97
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2025-04-02 10:37:15 +0900

    1013-FIBONACCI-NUMBER Stats: Time: 3 ms (69.53%), Space: 7.7 MB (88.21%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -1,26 +1,24 @@
-#include <stdint.h>
-#include <stdlib.h>
-
 int fib(int n) {
     // Base case
-    if (n == 0 || n == 1) {
-        return n;
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else if (n == 2) {
+        return 1;
     }
-
-    int64_t *fibonacciTable = (int64_t *)calloc((n + 1), sizeof(int64_t));
-    if (fibonacciTable == NULL) {
-        return -1;
-    }
-
-    // Base case
-    fibonacciTable[0] = 0;
-    fibonacciTable[1] = 1;
 
     // Calculate Fibonacci numbers iteratively
-    // instead of using recursion
-    for (int i = 2; i <= n; i++) {
-        fibonacciTable[i] = fibonacciTable[i - 1] + fibonacciTable[i - 2];
+    // Note that F(n) = F(n-1) + F(n-2)
+    int result = 0;
+    int i = 2;
+    int n1 = 1, n2 = 1; // F(1) = 1, F(2) = 1
+    while (i < n) {
+        result = n1 + n2; // F(i) = F(i-1) + F(i-2)
+        n1 = n2;          // Update F(i-1)
+        n2 = result;      // Update F(i)
+        i++;
     }
 
-    return fibonacciTable[n];
+    return result;
 }
